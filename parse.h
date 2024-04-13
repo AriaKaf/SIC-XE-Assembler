dda068307a1f60c47d6c80d0369d6da2f835d844
AriaKaf <146138103+AriaKaf@users.noreply.github.com>
2024-04-12 18:54:48 -0700

    Update parse.h

--- parse.h
+++ parse.h
@@ -4,6 +4,8 @@
 
 #include <string>
 
+bool is_a_comment(const std::string& line);
+
 std::string symbol_of(const std::string& line);
 std::string mnemonic_of(const std::string& line);
 std::string operand_of(const std::string& line);
