70053b450291e1a666e4d50276b11e6223da230b
AriaKaf <146138103+AriaKaf@users.noreply.github.com>
2024-04-17 16:27:19 -0700

    Update parse.cpp

--- src/parse.cpp
+++ src/parse.cpp
@@ -2,15 +2,108 @@
 #include "optab.h"
 #include "parse.h"
 
+#include <iomanip>
+#include <sstream>
+
+void trim(std::string& s) {
+  size_t begin = s.find_first_not_of(" \t\r");
+  size_t end = s.find_last_not_of(" \t\r");
+  s = s.substr(begin, end - begin + 1);
+}
+
+bool is_numeric(const std::string& s) {
+  for (char c : s)
+    if (!std::isdigit(c))
+      return false;
+  return true;
+}
+
+int evaluate_term(std::string term, const SymbolTable& stab, int locctr) {
+  trim(term);
+  if (term == "*")
+    return locctr;
+  if (is_numeric(term))
+    return std::stoi(term);
+  if (stab.contains(term))
+    return stab[term].value;
+}
+
+size_t least_precedence_operator_pos(const std::string& str) {
+  if (str.find_first_of("+-") != std::string::npos) {
+    for (int i = str.size() - 1; i >= 0; i--)
+      if (str[i] == '+' || str[i] == '-')
+	return i;
+  } else {
+    for (int i = str.size() - 1; i >= 0; i--)
+      if (str[i] == '*' || str[i] == '/')
+	return i;
+  }
+}
+
+Flag evaluate_flag(const std::string& expr, const SymbolTable& stab) {
+
+  int relative_additions = 0, relative_subtractions = 0;
+
+  std::stringstream token;
+  char c, last_operator;
+  std::istringstream is(expr);
+
+  is >> std::skipws;
+  
+  while (is >> c && std::string("*=-/").find(c) == std::string::npos)
+    token << c;
+  last_operator = c;
+
+  if (stab.contains(token.str()) && stab[token.str()].flag == RELATIVE)
+    relative_additions++;
+  token.str("");
+
+  while (is >> c) {
+    if (std::string("*+-/").find(c) != std::string::npos) {
+      if (stab.contains(token.str()) && stab[token.str()].flag == RELATIVE) {
+        if (last_operator == '+')
+          relative_additions++;
+        else if (last_operator == '-')
+          relative_subtractions++;
+      }
+      last_operator = c;
+      token.str("");
+    } else
+      token << c;
+  }
+
+  if (stab.contains(token.str()) && stab[token.str()].flag == RELATIVE) {
+    if (last_operator == '+')
+      relative_additions++;
+    else if (last_operator == '-')
+      relative_subtractions++;  
+  }
+  
+  return relative_additions == relative_subtractions ? ABSOLUTE : RELATIVE;
+}
+
+int evaluate_expr(const std::string& expr, const SymbolTable& stab, int locctr) {
+
+  if (expr.find_first_of("+*-/") == std::string::npos || expr.find_first_not_of("* \t\r") == std::string::npos)
+    return evaluate_term(expr, stab, locctr);
+
+  size_t split = least_precedence_operator_pos(expr);
+
+  switch (expr[split])
+  {
+    case '+': return evaluate_expr(expr.substr(0, split), stab, locctr) + evaluate_expr(expr.substr(split + 1), stab, locctr);
+    case '-': return evaluate_expr(expr.substr(0, split), stab, locctr) - evaluate_expr(expr.substr(split + 1), stab, locctr);
+    case '*': return evaluate_expr(expr.substr(0, split), stab, locctr) * evaluate_expr(expr.substr(split + 1), stab, locctr);
+    case '/': return evaluate_expr(expr.substr(0, split), stab, locctr) / evaluate_expr(expr.substr(split + 1), stab, locctr);
+  }
+}
+
 int ascii_to_int(const std::string& ascii) {
-
   int value = 0;
-
   for (char c : ascii) {
     value <<= 8;
     value |= static_cast<unsigned char>(c);
   }
-
   return value;
 }
 
@@ -22,7 +115,7 @@ int instruction_length_of(const std::string& line) {
     return mnemonic_prefix_of(line) == '+' ? 4 : optab[mnemonic].format;
   else if (mnemonic == "WORD")
     return 3;
-
+  
   std::string operand = operand_of(line);
 
   if (mnemonic == "RESW")
@@ -63,5 +156,9 @@ char operand_prefix_of(const std::string& line) {
 }
 
 std::string operand_of(const std::string& line) {
-  return line.size() < 19 || is_whitespace(line.substr(18)) ? "" : line.substr(18, line.substr(18).find_first_of(" \t\r"));
+
+  if (line.size() >= 42 && !is_whitespace(line.substr(41)))
+    return line.substr(18, line.substr(18, 23).find_last_not_of(" \t\r") + 1);
+  
+  return line.size() < 19 || is_whitespace(line.substr(18)) ? "" : line.substr(18, line.substr(18).find_last_not_of(" \t\r") + 1);
 }
